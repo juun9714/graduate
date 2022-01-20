829c3d72f606eb340a8d4299ee005a70274c4fa2
juun9714 <lovekwon200@gmail.com>
2022-01-20 19:59:27 +0900

    CORS bypass

--- graduate/src/components/Pict.js
+++ graduate/src/components/Pict.js
@@ -16,12 +16,21 @@ class Pict extends Component {
 
     render() {
 
-        // function getPost(){
-        //     axios.get("localhost:8000").then(response=>{
-        //         console.log(response)
-        //     })
-        // }
-        // getPost()
+        //http:// 추가 안해주면 network err 남
+
+        //"C:\Program Files\Google\Chrome\Application\chrome.exe" --disable-web-security --user-data-dir="C:\Temp"
+        //터미널에서 실행하면 불안전한 버전으로 크롬 켜지고, 그 크롬에서 리액트 실행하면, CORS 차단 안먹고 axios 실행됨
+
+        function getPost(){
+            axios.get("http://localhost:8000").then(response=>{
+                console.log(response)
+            }).catch((err)=>{
+                console.log(err)
+            })
+        }
+        getPost()
+
+
         var pict = null
         var pict_tit = null
         if (this.props.index === 1) {
